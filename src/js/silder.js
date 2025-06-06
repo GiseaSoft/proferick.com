4957fed5a4db92d8792a5da933e5c2e0e0d6ca55
Sergio Gómez <sergioagmoreno@gmail.com>
2025-06-06 11:14:12 -0500

    improved testimonials

--- src/js/silder.js
+++ src/js/silder.js
@@ -1,246 +1,238 @@
-const repeat = true;
-const noArrows = false;
-const noBullets = false;
+function ReviewsSlider() {
+    const _self = this;
+    
+    this.repeat = true;
+    this.arrows = true;
+    this.bullets = true;
 
-const container = document.querySelector('.slider-container');
-var slide = document.querySelectorAll('.slider-single');
-var slideTotal = slide.length - 1;
-var slideCurrent = -1;
+    this.container = document.querySelector('.slider-container');
+    this.slide = _self.container.querySelectorAll('.slider-single');
+    this.slideTotal = _self.slide.length - 1;
+    this.slideCurrent = -1;
 
-function initBullets() {
-    if (noBullets) {
-        return;
+    /* *************** */
+    /* Setup Functions */
+    /* *************** */
+
+    this.init = function() {
+        _self.initBullets();
+        _self.initArrows();
+        setTimeout(function () {
+            _self.slideRight();
+        }, 500);
     }
 
-    const bulletContainer = document.createElement('div');
-    bulletContainer.classList.add('bullet-container');
-
-    slide.forEach((elem, i) => {
-        const bullet = document.createElement('div');
-        bullet.classList.add('bullet')
-        bullet.id = `bullet-index-${i}`
-        bullet.addEventListener('click', () => {
-            goToIndexSlide(i);
-        })
-        bulletContainer.appendChild(bullet);
-        elem.classList.add('proactivede');
-    });
-
-    container.appendChild(bulletContainer);
-}
-
-function initArrows() {
-    if (noArrows) {
-        return;
+    this.initBullets = function() {
+        if (!_self.bullets) {
+            return;
+        }
+    
+        const bulletContainer = document.createElement('div');
+        bulletContainer.classList.add('bullet-container');
+    
+        _self.slide.forEach((elem, i) => {
+            const bullet = document.createElement('div');
+            bullet.classList.add('bullet')
+            bullet.id = `bullet-index-${i}`
+            bullet.addEventListener('click', () => {
+                _self.goToIndexSlide(i);
+            })
+            bulletContainer.appendChild(bullet);
+            elem.classList.add('proactivede');
+        });
+    
+        _self.container.appendChild(bulletContainer);
+    }
+    
+    this.initArrows = function() {
+        if (!_self.arrows) {
+            return;
+        }
+    
+        const leftArrow = document.createElement('button');
+        const iLeft = document.createElement('i');
+    
+        iLeft.classList.add('fa');
+        iLeft.classList.add('fa-arrow-left');
+    
+        leftArrow.classList.add('slider-left');
+        leftArrow.appendChild(iLeft);
+    
+        leftArrow.addEventListener('click', () => {
+            _self.slideLeft();
+        });
+    
+        const rightArrow = document.createElement('button');
+        const iRight = document.createElement('i');
+    
+        iRight.classList.add('fa');
+        iRight.classList.add('fa-arrow-right');
+    
+        rightArrow.classList.add('slider-right');
+        rightArrow.appendChild(iRight);
+    
+        rightArrow.addEventListener('click', () => {
+            _self.slideRight();
+        });
+    
+        _self.container.appendChild(leftArrow);
+        _self.container.appendChild(rightArrow);
     }
 
-    const leftArrow = document.createElement('button');
-    const iLeft = document.createElement('i');
-
-    iLeft.classList.add('fa');
-    iLeft.classList.add('fa-arrow-left');
-
-    leftArrow.classList.add('slider-left');
-    leftArrow.appendChild(iLeft);
-
-    leftArrow.addEventListener('click', () => {
-        slideLeft();
-    });
-
-    const rightArrow = document.createElement('button');
-    const iRight = document.createElement('i');
-
-    iRight.classList.add('fa');
-    iRight.classList.add('fa-arrow-right');
-
-    rightArrow.classList.add('slider-right');
-    rightArrow.appendChild(iRight);
-
-    rightArrow.addEventListener('click', () => {
-        slideRight();
-    });
-
-    container.appendChild(leftArrow);
-    container.appendChild(rightArrow);
-}
-
-function slideInitial() {
-    initBullets();
-    initArrows();
-    setTimeout(function () {
-        slideRight();
-    }, 500);
-}
-
-function updateBullet() {
-    if (!noBullets) {
-        document.querySelector('.bullet-container').querySelectorAll('.bullet').forEach((elem, i) => {
+    /* **************** */
+    /* Update Functions */
+    /* **************** */
+    
+    this.updateBullet = function() {
+        _self.container.querySelector('.bullet-container').querySelectorAll('.bullet').forEach((elem, i) => {
             elem.classList.remove('active');
-            if (i === slideCurrent) {
+            if (i === _self.slideCurrent) {
                 elem.classList.add('active');
             }
         })
-    }
-    checkRepeat();
-}
 
-function checkRepeat() {
-    if (!repeat) {
-        if (slideCurrent === slide.length - 1) {
-            slide[0].classList.add('not-visible');
-            slide[slide.length - 1].classList.remove('not-visible');
-            if (!noArrows) {
-                document.querySelector('.slider-right').classList.add('not-visible');
-                document.querySelector('.slider-left').classList.remove('not-visible');
+        if (!_self.repeat) {
+            _self.stopRepeat();
+        }
+    }
+    
+    this.stopRepeat = function() {
+        if (_self.slideCurrent === _self.slide.length - 1) {
+            _self.slide[0].classList.add('not-visible');
+            _self.slide[_self.slide.length - 1].classList.remove('not-visible');
+            if (!_self.arrows) {
+                _self.container.querySelector('.slider-right').classList.add('not-visible');
+                _self.container.querySelector('.slider-left').classList.remove('not-visible');
             }
         }
-        else if (slideCurrent === 0) {
-            slide[slide.length - 1].classList.add('not-visible');
-            slide[0].classList.remove('not-visible');
+        else if (_self.slideCurrent === 0) {
+            _self.slide[_self.slide.length - 1].classList.add('not-visible');
+            _self.slide[0].classList.remove('not-visible');
 
-            if (!noArrows) {
-                document.querySelector('.slider-left').classList.add('not-visible');
-                document.querySelector('.slider-right').classList.remove('not-visible');
+            if (!_self.arrows) {
+                _self.container.querySelector('.slider-left').classList.add('not-visible');
+                _self.container.querySelector('.slider-right').classList.remove('not-visible');
             }
 
         } else {
-            slide[slide.length - 1].classList.remove('not-visible');
-            slide[0].classList.remove('not-visible');
+            _self.slide[_self.slide.length - 1].classList.remove('not-visible');
+            _self.slide[0].classList.remove('not-visible');
 
-            if (!noArrows) {
-                document.querySelector('.slider-left').classList.remove('not-visible');
-                document.querySelector('.slider-right').classList.remove('not-visible');
+            if (!_self.arrows) {
+                _self.container.querySelector('.slider-left').classList.remove('not-visible');
+                _self.container.querySelector('.slider-right').classList.remove('not-visible');
             }
         }
     }
-}
-
-function slideRight() {
-    if (slideCurrent < slideTotal) {
-        slideCurrent++;
-    } else {
-        slideCurrent = 0;
-    }
-
-    if (slideCurrent > 0) {
-        var preactiveSlide = slide[slideCurrent - 1];
-    } else {
-        var preactiveSlide = slide[slideTotal];
-    }
-    var activeSlide = slide[slideCurrent];
-
-    if (slideCurrent < slideTotal) {
-        var proactiveSlide = slide[slideCurrent + 1];
-    } else {
-        var proactiveSlide = slide[0];
-
-    }
-
-    slide.forEach((elem) => {
-        var thisSlide = elem;
-        if (thisSlide.classList.contains('preactivede')) {
-            thisSlide.classList.remove('preactivede');
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.add('proactivede');
+    
+    this.slideRight = function() {
+        if (_self.slideCurrent < _self.slideTotal) {
+            _self.slideCurrent++;
+        } else {
+            _self.slideCurrent = 0;
         }
-        if (thisSlide.classList.contains('preactive')) {
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.remove('proactivede');
-            thisSlide.classList.add('preactivede');
+    
+        if (_self.slideCurrent > 0) {
+            var preactiveSlide = _self.slide[_self.slideCurrent - 1];
+        } else {
+            var preactiveSlide = _self.slide[_self.slideTotal];
         }
-    });
+    
+        if (_self.slideCurrent < _self.slideTotal) {
+            var proactiveSlide = _self.slide[_self.slideCurrent + 1];
+        } else {
+            var proactiveSlide = _self.slide[0];
+    
+        }
+    
+        _self.slide.forEach((elem) => {
+            var thisSlide = elem;
+            if (thisSlide.classList.contains('preactivede')) {
+                _self.cleanClasses(thisSlide);
+                thisSlide.classList.add('proactivede');
+            }
+            if (thisSlide.classList.contains('preactive')) {
+                _self.cleanClasses(thisSlide);
+                thisSlide.classList.add('preactivede');
+            }
+        });
 
-    preactiveSlide.classList.remove('preactivede');
-    preactiveSlide.classList.remove('active');
-    preactiveSlide.classList.remove('proactive');
-    preactiveSlide.classList.remove('proactivede');
-    preactiveSlide.classList.add('preactive');
-
-    activeSlide.classList.remove('preactivede');
-    activeSlide.classList.remove('preactive');
-    activeSlide.classList.remove('proactive');
-    activeSlide.classList.remove('proactivede');
-    activeSlide.classList.add('active');
-
-    proactiveSlide.classList.remove('preactivede');
-    proactiveSlide.classList.remove('preactive');
-    proactiveSlide.classList.remove('active');
-    proactiveSlide.classList.remove('proactivede');
-    proactiveSlide.classList.add('proactive');
-
-    updateBullet();
-}
-
-function slideLeft() {
-    if (slideCurrent > 0) {
-        slideCurrent--;
-    } else {
-        slideCurrent = slideTotal;
+        _self.updateSlides(preactiveSlide, proactiveSlide);
     }
-
-    if (slideCurrent < slideTotal) {
-        var proactiveSlide = slide[slideCurrent + 1];
-    } else {
-        var proactiveSlide = slide[0];
-    }
-
-    var activeSlide = slide[slideCurrent];
-
-    if (slideCurrent > 0) {
-        var preactiveSlide = slide[slideCurrent - 1];
-    } else {
-        var preactiveSlide = slide[slideTotal];
-    }
-
-    slide.forEach((elem) => {
-        var thisSlide = elem;
+    
+    this.slideLeft = function() {
+        if (_self.slideCurrent > 0) {
+            _self.slideCurrent--;
+        } else {
+            _self.slideCurrent = _self.slideTotal;
+        }
+    
+        if (_self.slideCurrent < _self.slideTotal) {
+            var proactiveSlide = _self.slide[_self.slideCurrent + 1];
+        } else {
+            var proactiveSlide = _self.slide[0];
+        }
+    
+        if (_self.slideCurrent > 0) {
+            var preactiveSlide = _self.slide[_self.slideCurrent - 1];
+        } else {
+            var preactiveSlide = _self.slide[_self.slideTotal];
+        }
+    
+        _self.slide.forEach((elem) => {
+            var thisSlide = elem;
+            
+            if (thisSlide.classList.contains('proactive')) {
+                _self.cleanClasses(thisSlide);
+                thisSlide.classList.add('proactivede');
+            }
+            if (thisSlide.classList.contains('proactivede')) {
+                _self.cleanClasses(thisSlide);
+                thisSlide.classList.add('preactivede');
+            }
+        });
         
-        if (thisSlide.classList.contains('proactive')) {
-            thisSlide.classList.remove('preactivede');
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.add('proactivede');
+        
+        _self.updateSlides(preactiveSlide, proactiveSlide);
+    }
+
+    this.goToIndexSlide = function(index) {
+        const sliding = (_self.slideCurrent > index) ? () => _self.slideRight() : () => _self.slideLeft();
+        while (_self.slideCurrent !== index) {
+            sliding();
         }
-        if (thisSlide.classList.contains('proactivede')) {
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.remove('proactivede');
-            thisSlide.classList.add('preactivede');
+    }
+
+    /* **************** */
+    /* Helper Functions */
+    /* **************** */
+
+    this.updateSlides = function(preactiveSlide, proactiveSlide) {
+        var activeSlide = _self.slide[_self.slideCurrent];
+
+        _self.cleanClasses(preactiveSlide);
+        preactiveSlide.classList.add('preactive');
+    
+        _self.cleanClasses(activeSlide);
+        activeSlide.classList.add('active');
+    
+        _self.cleanClasses(proactiveSlide);
+        proactiveSlide.classList.add('proactive');
+    
+        
+        if (_self.bullets) {
+            _self.updateBullet();
         }
-    });
+    }
 
-    preactiveSlide.classList.remove('preactivede');
-    preactiveSlide.classList.remove('active');
-    preactiveSlide.classList.remove('proactive');
-    preactiveSlide.classList.remove('proactivede');
-    preactiveSlide.classList.add('preactive');
-
-    activeSlide.classList.remove('preactivede');
-    activeSlide.classList.remove('preactive');
-    activeSlide.classList.remove('proactive');
-    activeSlide.classList.remove('proactivede');
-    activeSlide.classList.add('active');
-
-    proactiveSlide.classList.remove('preactivede');
-    proactiveSlide.classList.remove('preactive');
-    proactiveSlide.classList.remove('active');
-    proactiveSlide.classList.remove('proactivede');
-    proactiveSlide.classList.add('proactive');
-
-    updateBullet();
-}
-
-function goToIndexSlide(index) {
-    const sliding = (slideCurrent > index) ? () => slideRight() : () => slideLeft();
-    while (slideCurrent !== index) {
-        sliding();
+    this.cleanClasses = function(elem) {
+        elem.classList.remove('preactivede');
+        elem.classList.remove('preactive');
+        elem.classList.remove('active');
+        elem.classList.remove('proactive');
+        elem.classList.remove('proactivede');
     }
 }
 
-slideInitial();
+const reviewsSlider = new ReviewsSlider();
+reviewsSlider.init();
